8346b470f1eeb420b90f6c0c084f311b35c4b636
Israel Huerta <israelhf24@gmail.com>
2025-01-21 15:48:02 -0600

    update security configuration to allow specific endpoints and permit all for others

--- src/main/java/com/huerta/bank_application/config/ProjectSecurityConfig.java
+++ src/main/java/com/huerta/bank_application/config/ProjectSecurityConfig.java
@@ -9,12 +9,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        return http.build();
-    }
-
+  @Bean
+  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+        (requests) ->
+            requests
+                .requestMatchers("api/v1/account", "api/v1/balance", "api/v1/loan", "api/v1/card")
+                .authenticated()
+                .requestMatchers("api/v1/notice", "api/v1/contact", "/error")
+                .permitAll());
+    http.formLogin(withDefaults());
+    http.httpBasic(withDefaults());
+    return http.build();
+  }
 }
