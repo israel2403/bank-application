88a633d80d8ee4e3495a6abbd791505a88438f37
Israel Huerta <israelhf24@gmail.com>
2025-04-15 07:57:00 -0600

    refactor: clean up ProjectSecurityConfig by removing unused imports and comments

--- src/main/java/com/huerta/bank_application/config/ProjectSecurityConfig.java
+++ src/main/java/com/huerta/bank_application/config/ProjectSecurityConfig.java
@@ -2,37 +2,26 @@ package com.huerta.bank_application.config;
 
 import static org.springframework.security.config.Customizer.*;
 
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
-        (requests) ->
-            requests
-                .requestMatchers("api/v1/account", "api/v1/balance", "api/v1/loan", "api/v1/card")
-                .authenticated()
-                .requestMatchers("api/v1/notice", "api/v1/contact", "/error")
-                .permitAll());
+        (requests) -> requests
+            .requestMatchers("api/v1/account", "api/v1/balance", "api/v1/loan", "api/v1/card")
+            .authenticated()
+            .requestMatchers("api/v1/notice", "api/v1/contact", "/error")
+            .permitAll());
     http.formLogin(withDefaults());
     http.httpBasic(withDefaults());
     return http.build();
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService(final DataSource dataSource) {
-    return new JdbcUserDetailsManager(dataSource);
   }
 
   @Bean
@@ -40,13 +29,4 @@ public class ProjectSecurityConfig {
     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 
-  /**
-   * From Spring Security 6.3
-   *
-   * @return
-   */
-  @Bean
-  public CompromisedPasswordChecker compromisedPasswordChecker() {
-    return new HaveIBeenPwnedRestApiPasswordChecker();
-  }
 }
